c7f934da3522ed4205847e138a69afc1fea2b07f
Suresh Kumar Nagendran <surnagen1@publicisgroupe.net>
2023-01-03 10:02:37 -0600

    updated zuul config to route

--- zuul-gateway/src/main/java/com/example/demo/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/demo/ZuulGatewayApplication.java
@@ -3,9 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
-@EnableZuulServer
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
